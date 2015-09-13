a5989a802500612ca3a7089b3b3661a6bb9fbaf1
yantene <contact@yantene.net>
2015-09-13 18:54:49 +0900

    output/ ディレクトリへ出力するようにした

--- generate.sh
+++ generate.sh
@@ -2,9 +2,20 @@
 
 SVG_FILE="yantene.svg"
 
-# Generate 512 x 512 icon
+# Make and clean dist directory
 
-ICON_FILE_NAME="icon.png"
+mkdir -p output
+rm -rf output/*
+
+# Generate plain SVG
+
+SVG_FILE_NAME="output/yantene.svg"
+
+inkscape $SVG_FILE \
+  -l=$SVG_FILE_NAME \
+  -z
+
+# Generate 512 x 512 icon
 
 ICON_W=512.0
 ICON_H=512.0
@@ -12,6 +23,8 @@ ICON_X0=1307.988
 ICON_Y0=189.988
 ICON_X1=1626.442
 ICON_Y1=508.442
+
+ICON_FILE_NAME="output/icon.png"
 
 inkscape $SVG_FILE \
   -a=$ICON_X0:$ICON_Y0:$ICON_X1:$ICON_Y1 \
@@ -21,7 +34,7 @@ inkscape $SVG_FILE \
 
 # Generate full image
 
-FULL_FILE_NAME="full.png"
+FULL_FILE_NAME="output/full.png"
 
 inkscape $SVG_FILE \
   -e=$FULL_FILE_NAME \
